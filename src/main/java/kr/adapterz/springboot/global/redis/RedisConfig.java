d26745082530cee8c5ea8056de0f7fa1463fef36
Jinhyeok Kim <jinxxang123@gmail.com>
2026-08-08 18:53:09 +0900

    daily, weekly 기간별 step 분리

--- src/main/java/kr/adapterz/springboot/global/redis/RedisConfig.java
+++ src/main/java/kr/adapterz/springboot/global/redis/RedisConfig.java
@@ -3,11 +3,13 @@ package kr.adapterz.springboot.global.redis;
 import kr.adapterz.springboot.chat.pubsub.ChatMessageSubscriber;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 @Configuration
+@Profile("!h2")
 public class RedisConfig {
 
     private static final String CHAT_MESSAGE_CHANNEL =
